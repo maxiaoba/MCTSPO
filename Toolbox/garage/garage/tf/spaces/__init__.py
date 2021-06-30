79168e9031a324fe636d5726832db26eb32e1e33
Xiaobai Ma <xiaobaima@DNab421bb2.stanford.edu>
2021-06-30 14:07:58 -0700

    remove Dict spaces

--- Toolbox/garage/garage/tf/spaces/__init__.py
+++ Toolbox/garage/garage/tf/spaces/__init__.py
@@ -1,7 +1,7 @@
 """TensorFlow extension of garage.spaces."""
 
 from garage.tf.spaces.box import Box
-from garage.tf.spaces.dict import Dict
+# from garage.tf.spaces.dict import Dict
 from garage.tf.spaces.discrete import Discrete
 from garage.tf.spaces.tuple import Tuple
 
